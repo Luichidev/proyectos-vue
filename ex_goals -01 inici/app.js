7dcebf397ad7012da733fa02f3d7711e4232f138
@LuisArana <disconluis@gmail.com>
2022-03-30 13:08:10 +0200

    feat: Add exersice 01

--- ex_goals -01 inici/app.js
+++ ex_goals -01 inici/app.js
@@ -1,13 +1,32 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// const buttonEl = document.querySelector('button')
+// const inputEl = document.querySelector('input')
+// const listEl = document.querySelector('ul')
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value
+//   const listItemEl = document.createElement('li')
+//   listItemEl.textContent = enteredValue
+//   listEl.appendChild(listItemEl)
+//   inputEl.value = ''
+// }
 
-buttonEl.addEventListener('click', addGoal);
+// buttonEl.addEventListener('click', addGoal)
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    goal: [],
+    goalInput: '',
+  },
+  methods: {
+    addGoal() {
+      this.goal.push(this.goalInput)
+      this.goalInput = ''
+      console.log(this.goal)
+    },
+    removeGoal(goal) {
+      this.goal.splice(this.goal.indexOf(goal), 1)
+      console.log(this.goal)
+    },
+  },
+})
