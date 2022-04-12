ecb478722f516efb2b6797498e5a6c2e6edbd138
@LuisArana <disconluis@gmail.com>
2022-04-12 18:14:15 +0200

    feat: Create Vue app

---
+++ Consolidacio-Vue/js/index.js
@@ -0,0 +1,44 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    // Data
+    frases: [
+      {
+        id: 1,
+        frase:
+          'Así es cómo se gana una guerra. No luchando contra lo que odias, sino salvando lo que amas.',
+        autor: '- Roste Tico',
+      },
+      {
+        id: 2,
+        frase: '¿Quién es más loco: el loco o el loco que sigue al loco?',
+        autor: '- Obi-Wan',
+      },
+      {
+        id: 3,
+        frase: 'La guerra no lo hace a uno más grandioso.',
+        autor: '- Yoda a Luke Skywalker',
+      },
+      {
+        id: 4,
+        frase:
+          'Concéntrate en el momento. Siente, no pienses, usa tu instinto.',
+        autor: '- Qui-Gon Jinn a Anakin Skywalker',
+      },
+      {
+        id: 5,
+        frase:
+          'El miedo es el camino hacia el Lado Oscuro. El miedo lleva a la ira, la ira lleva al odio, el odio lleva al sufrimiento. Percibo mucho miedo en ti.',
+        autor: '- Maestro Yoda',
+      },
+      {
+        id: 6,
+        frase: 'No. No lo intentes. Hazlo, o no lo hagas, pero no lo intentes.',
+        autor: '- Maestro Yoda',
+      },
+    ],
+  },
+  methods: {
+    // Methods
+  },
+})
