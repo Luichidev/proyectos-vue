38b6332e0229b313d37b419386e4e5143c255566
luichidev <disconluis@gmail.com>
2022-04-19 14:03:03 +0200

    feat: Add component HomeUser

---
+++ vue-cli/test1/src/components/HomeUser.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <div class="user-info">
+      <h1>Hello</h1>
+      <p></p>
+      <div class="socials"></div>
+    </div>
+    <div class="user-image">
+      <img src="#" alt="User avatar">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomeUser',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
