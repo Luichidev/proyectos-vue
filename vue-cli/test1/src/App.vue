3a82e005c9ef616f1aafaccb3abf48495ece570d
luichidev <disconluis@gmail.com>
2022-04-19 14:03:14 +0200

    feat: Add new project

---
+++ vue-cli/test1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HomeUser></HomeUser>
+</template>
+
+<script>
+import HomeUser from './components/HomeUser.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomeUser
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
