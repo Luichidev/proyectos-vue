30c6366bd253b833654b6b294fa1c6f2101e22bc
luichidev <disconluis@gmail.com>
2022-04-21 13:09:04 +0200

    feat: add component HomeUser

---
+++ vue-cli/porfolio/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <HomeUser/>
+  </section>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeUser from '@/components/HomeUser.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HomeUser
+  }
+}
+</script>
